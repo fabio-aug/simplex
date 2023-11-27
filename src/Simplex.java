2f72ca9bb30673c39675a79528da1c420637366c
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2023-11-27 08:13:47 -0300

    Update variables names

--- src/Simplex.java
+++ src/Simplex.java
@@ -4,79 +4,79 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Simplex {
-    private boolean ehMaximizacao;
-    private int numeroVariaveis;
-    private int numeroRestricoes;
-    private float[][] matriz;
+    private boolean isMax;
+    private int numVariables;
+    private int numRestrictions;
+    private float[][] matrix;
     private int[] base;
 
-    public Simplex(String nomeArquivo) {
-        lerArquivoEntrada(nomeArquivo);
+    public Simplex(String fileName) {
+        readFile(fileName);
     }
 
-    public void lerArquivoEntrada(String nomeArquivo) {
+    public void readFile(String fileName) {
         Scanner reader;
         try {
-            reader = new Scanner(new File(nomeArquivo));
+            reader = new Scanner(new File(fileName));
         } catch (FileNotFoundException e) {
             System.out.println("Arquivo não encontrado!");
             e.printStackTrace();
             return;
         }
 
-        this.ehMaximizacao = Integer.parseInt(reader.nextLine()) > 0;
-        this.numeroVariaveis = Integer.parseInt(reader.nextLine());
-        this.numeroRestricoes = Integer.parseInt(reader.nextLine());
-        this.base = new int[this.numeroVariaveis + this.numeroRestricoes];
+        this.isMax = Integer.parseInt(reader.nextLine()) > 0;
+        this.numVariables = Integer.parseInt(reader.nextLine());
+        this.numRestrictions = Integer.parseInt(reader.nextLine());
+        this.base = new int[this.numVariables + this.numRestrictions];
         for (int i = 0; i < this.base.length; i++)
-            this.base[i] = (i < this.numeroVariaveis) ? 0 : 1;
+            this.base[i] = (i < this.numVariables) ? 0 : 1;
 
-        this.matriz = new float[this.numeroRestricoes + 1][this.numeroVariaveis + this.numeroRestricoes + 1];
+        this.matrix = new float[this.numRestrictions + 1][this.numVariables + this.numRestrictions + 1];
 
-        for (int i = 0; i < this.numeroRestricoes + 1; i++) {
+        for (int i = 0; i < this.numRestrictions + 1; i++) {
             String data = reader.nextLine();
             String[] numeros = data.split(" ");
 
-            for (int j = 0; j < this.matriz[i].length; j++) {
-                if (j < this.numeroVariaveis) {
-                    this.matriz[i][j] = Float.parseFloat(numeros[j]);
+            for (int j = 0; j < this.matrix[i].length; j++) {
+                if (j < this.numVariables) {
+                    this.matrix[i][j] = Float.parseFloat(numeros[j]);
                     if (i == 0)
-                        this.matriz[i][j] = this.matriz[i][j] * -1;
+                        this.matrix[i][j] = this.matrix[i][j] * -1;
                 } else {
                     if (i != 0)
-                        this.matriz[i][this.numeroVariaveis + i - 1] = 1;
-                    this.matriz[i][j] = 0;
+                        this.matrix[i][this.numVariables + i - 1] = 1;
+                    this.matrix[i][j] = 0;
                 }
                 if (i != 0)
-                    this.matriz[i][this.matriz[i].length - 1] = Float.parseFloat(numeros[numeros.length - 1]);
+                    this.matrix[i][this.matrix[i].length - 1] = Float.parseFloat(numeros[numeros.length - 1]);
             }
         }
         reader.close();
     }
 
-    public int pegarColunaPivo() {
+    public int pickMinColumn() {
         int column = -1;
         float valor = 0;
-        for (int i = 0; i < matriz[0].length; i++) {
-            if (ehMaximizacao) {
-                if (matriz[0][i] < 0 && i == 0) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (isMax) {
+                if (matrix[0][i] < 0 && i == 0) {
                     column = i;
-                    valor = matriz[0][i];
+                    valor = matrix[0][i];
                 }
 
-                if (column != -1 && matriz[0][i] < valor) {
+                if (column != -1 && matrix[0][i] < valor) {
                     column = i;
-                    valor = matriz[0][i];
+                    valor = matrix[0][i];
                 }
             } else {
-                if (matriz[0][i] < 0 && i == 0) {
+                if (matrix[0][i] < 0 && i == 0) {
                     column = i;
-                    valor = matriz[0][i];
+                    valor = matrix[0][i];
                 }
 
-                if (column != -1 && matriz[0][i] > valor && matriz[0][i] < 0) {
+                if (column != -1 && matrix[0][i] > valor && matrix[0][i] < 0) {
                     column = i;
-                    valor = matriz[0][i];
+                    valor = matrix[0][i];
                 }
             }
         }
@@ -86,8 +86,8 @@ public class Simplex {
     @Override
     public String toString() {
         String print = "";
-        for (int i = 0; i < this.matriz.length; i++) {
-            print += Arrays.toString(this.matriz[i]) + "\n";
+        for (int i = 0; i < this.matrix.length; i++) {
+            print += Arrays.toString(this.matrix[i]) + "\n";
         }
         return print;
     }
